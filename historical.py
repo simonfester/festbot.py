1696b5bded6878ee40e22593df0c11a42e3d9eba
simonfester <syhester@gmail.com>
2020-07-11 17:47:29 +0700

    evening updates

--- historical.py
+++ historical.py
@@ -1,50 +1,49 @@
 # historical.py to get historical data from Binance
 
 from datetime import datetime
+import requests                     # https://requests.readthedocs.io/en/master/user/quickstart/
 
-# https://requests.readthedocs.io/en/master/user/quickstart/
-import requests
+timeout = 10 # for the request request
 
-# symbol we are using
+# required kline values
 symbol = 'BTCUSDT'
-
-# required kline interval
 interval = '1d'
-
-# start time in ms
-start_time = 0
+start_time = 0    # start time in ms
 limit = 1
 
-# base url
-base_url = 'https://api.binance.com'
+# urls
+base_url = 'https://api.binance.com'   # base url, all others start from here
+get_ping = '/api/v3/ping'              # ping endpoint
+get_time = '/api/v3/time'              # server time
+get_klines = '/api/v3/klines'          # kline endpoint
 
-# ping endpoint
-get_ping = '/api/v3/ping'
+# function to check if the server is connected
 
-# server time
-get_time = '/api/v3/time'
+def are_we_connected():
+     ping = requests.get(base_url+get_ping)
+     if '{}' in ping.text:
+         print('Connected')
+     else:
+         print('Error not connected')
 
-# kline endpoint
-get_klines = '/api/v3/klines'
+# function to get server time 
 
+def get_server_time():
+     time = requests.get(base_url + get_time)
+     time_dict = time.json()
+     server_time_in_ms = (time_dict['serverTime'])
+     return datetime.fromtimestamp(server_time_in_ms / 1000)
 
-# check server is connected
-ping = requests.get(base_url+get_ping)
-if '{}' in ping.text:
-    print('Connected')
-else:
-    print('Error not connected')
+# get symbol start time
 
-# request server time, convert to dictionary and then print
-time = requests.get(base_url+get_time)
-time_dict = time.json()
-server_time_in_ms = (time_dict['serverTime'])
-date = datetime.fromtimestamp(server_time_in_ms / 1000)
-print('Server Time: ',date)
+def get_symbol_start_time():
+     klines = requests.get(base_url+get_klines, params={'symbol': symbol, 'interval': interval, 'limit': limit, 'startTime': start_time}, timeout = timeout)
+     return(klines.json())
 
-# get klines
-klines = requests.get(base_url+get_klines, params={'symbol': symbol, 'interval': interval, 'limit': limit, 'startTime': start_time})
-print(klines.text)
+start_time = get_symbol_start_time()[0]
+print(start_time[0])
+
+# kline json return structure
 
 #  1499040000000,      // Open time
 #  "0.01634790",       // Open
@@ -57,4 +56,4 @@ print(klines.text)
 #  308,                // Number of trades
 #  "1756.87402397",    // Taker buy base asset volume
 #  "28.46694368",      // Taker buy quote asset volume
-#  "17928899.62484339" // Ignore.  
+#  "17928899.62484339" // Ignore. 
