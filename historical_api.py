4112eb8feb689635565008d7684e19feb8345275
simonfester <syhester@gmail.com>
2020-07-12 12:43:23 +0700

    added helpers.py

--- historical_api.py
+++ historical_api.py
@@ -1,6 +1,34 @@
 import os
+import json
 from binance.client import Client
+from dotenv import load_dotenv
+from helpers import date_to_milliseconds
+
+
+project_folder = os.path.expanduser('~/pyhton')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
+
+api_key = os.getenv("api_key")
+api_secret = os.getenv("api_secret")
+
 client = Client(api_key, api_secret)
 
-klines = client.get_historical_klines("BNBBTC", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
-print(os.environ)
+symbol = "ETHBTC"
+start = "1 Dec, 2017"
+end = "1 Jan, 2020"
+interval = Client.KLINE_INTERVAL_30MINUTE
+
+klines = client.get_historical_klines(symbol, interval, start, end)
+
+# open a file with filename including symbol, interval and start and end converted to milliseconds
+with open(
+    "Binance_{}_{}_{}-{}.json".format(
+        symbol, 
+        interval, 
+        date_to_milliseconds(start),
+        date_to_milliseconds(end)
+    ),
+    'w' # set file write mode
+) as f:
+    f.write(json.dumps(klines))
+
