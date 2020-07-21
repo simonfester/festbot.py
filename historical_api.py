99346f0c7b59b3c4281e8567dd87d9ba8da55a0e
simonfester <syhester@gmail.com>
2020-07-21 10:14:31 +0700

    removed empty line

--- historical_api.py
+++ historical_api.py
@@ -1,9 +1,10 @@
 import os
+import sys
 import json
+
 from binance.client import Client
 from dotenv import load_dotenv
 from helpers import date_to_milliseconds
-
 
 project_folder = os.path.expanduser('~/pyhton')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
@@ -13,10 +14,26 @@ api_secret = os.getenv("api_secret")
 
 client = Client(api_key, api_secret)
 
-symbol = "ETHBTC"
+index = 0
+index_length = 856 #hack for now ... index_length = (len(supported_symbols['symbols'][856]))
+supported_symbols = client.get_exchange_info()
+list = []
+
+while index < index_length:
+    list.append((supported_symbols['symbols'][index]['symbol']))
+    index = index + 1
+
 start = "1 Dec, 2017"
-end = "1 Jan, 2020"
+end = "2 Dec, 2017"
 interval = Client.KLINE_INTERVAL_30MINUTE
+
+# get symbol
+
+print("Please enter a supported symbol: ETHBTC, BTCUSDT")
+symbol = input()
+if symbol not in list:
+    print("Exiting .. Not a valid symbol")
+    sys.exit()
 
 klines = client.get_historical_klines(symbol, interval, start, end)
 
